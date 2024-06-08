ad589f7045f0ef1a8ad101d86579a6cc27aed3aa
orezsuxen <none@no.nil>
2024-06-08 17:33:08 +0200

    whichkey and lualine updates

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -106,7 +106,7 @@ return {
 			format = "{kind_icon}{symbol.name:Normal}",
 			-- The following line is needed to fix the background color
 			-- Set it to the lualine section you want to use
-			hl_group = "lualine_c_normal",
+			-- hl_group = "lualine_c_normal",
 		})
 		table.insert(opts.sections.lualine_c, {
 			symbols.get,
--- nvim/lua/plugins/wichkey.lua
+++ nvim/lua/plugins/wichkey.lua
@@ -1,22 +1,19 @@
 return { -- Useful plugin to show you pending keybinds.
 	"folke/which-key.nvim",
 	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
+	opts = {
+		window = {
+			border = "single",
+			margin = { 0, 0, 0, 0 }, -- top, right , bottom, left
+			padding = { 0, 0, 1, 0 },
+		},
+	},
+	config = function(_, opts) -- This is the function that runs, AFTER loading
+		require("which-key").setup(opts)
 
 		-- Document existing key chains
 		require("which-key").register({
-			-- form kickstart
-			-- ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			-- ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			-- ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			-- ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			-- ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			-- ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			-- ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			-- from me
 			["<leader>c"] = { name = "[c]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[d]ocument", _ = "which_key_ignore" },
 			["<leader>G"] = { name = "[G]it Repository", _ = "which_key_ignore" },
 			["<leader>g"] = { name = "[g]it basics", _ = "which_key_ignore" },
 			["<leader><tab>"] = { name = "[Tab]Tabs", _ = "which_key_ignore" },
